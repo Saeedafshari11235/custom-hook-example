c60f8726b8404463c5c55067de149764dc81f391
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-19 11:10:11 +0330

    create a fetch hook

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+export default function useFetch() {
+  const [posts, setPosts] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json)
+        .then((data) => {
+          setPosts(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
+
+  return [posts, isPending, error];
+}
